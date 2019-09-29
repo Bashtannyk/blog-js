d3e8852e239b8a349c0b5343b6b504b588098e60
plyavinskiy <klerk2012@gmail.com>
2019-09-29 15:33:01 +0300

    v2

---
+++ cart.js
@@ -0,0 +1,59 @@
+'use strict'
+
+
+const ar = [
+    {id:1, name: 'prod-1'}, 
+    {id:2, name: 'prod-2'},
+    {id:3, name: 'prod-3'},
+    {id:4, name: 'prod-4'},
+    {id:5, name: 'prod-5'},
+]
+
+const container = document.querySelector('#products');
+
+const cart = document.querySelector('#cart');
+
+ar.forEach(product => {
+    const wrap = document.createElement('div');
+    wrap.className = 'product';
+
+    wrap.onclick = function () {
+        const id = +this.dataset.productId;
+        const product = ar.find(p => p.id === id);
+        console.log();
+    }
+
+    wrap.dataset.productId = product.id;
+
+    const h2 = document.createElement('h2');
+
+    h2.innerHTML = product.name;
+
+    wrap.appendChild(h2);
+
+    container.appendChild(wrap);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const ar = ['1', '2'];
+
+
+let ar2 = ar;
+
+ar2.push = '312312';
+
+ar2 = {name: 'Vasa'};
+
+console.log(ar);
+*/
---
+++ test.js
@@ -0,0 +1,99 @@
+'use strict'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function hi (message) {
+    return 'Hello ' + message;
+
+}
+
+var hi2 = message => 'hello ' + message;
+
+console.log(hi2('User'));
+
+*/
+/*
+function User (name) {
+    this.name = name;
+    this.age = 25;
+    this.showName = function () {
+        alert(this.name);
+    }
+
+}
+var car = {
+    name: 'car'
+}
+
+var user1 = new User ('Vasa-1');
+var user2 = new User ('Piter-2');
+
+user1.showName();
+user2.showName();
+//console.log(user1);
+
+
+
+car.show = user1.showName;
+car.show();
+
+
+
+
+*/
+
+
+
+/*
+(function () {
+    function send () {
+        console.log('111')
+    }
+
+    var user = {name:'Pit', age:25}
+    user.pay = function () {
+        send();
+    }
+    window.myUser = user;
+})();
+
+myUser.pay();
+
+*/
